c1445106bce6efed08dc491f48e0f89c12f53253
nick naglieri <nick331102@yahoo.com>
2023-03-20 20:10:34 -0400

    Refactor Quiz class to support more extensive quiz data
    - Modify the __init__ method to accept a list of dictionaries representing the quiz questions and answers
    - Modify the add_question method to accept a dictionary containing the question and answer data
    - Modify the take_quiz method to correctly track the number of correct answers
    
    The Quiz class now supports quiz data with more extensive question and answer data, including multiple answer choices and correct answers at different indices.

--- quiz.py
+++ quiz.py
@@ -1,14 +1,12 @@
 class Quiz:
     def __init__(self, questions, answers):
-        self.questions = []
-        self.answers = []
+        self.questions = questions
+        self.answers = answers
     
-    # take a question and answer as parameters, and append to their respective lists
-    def add_question(self, question, answer):
-        self.questions.append(question)
-        self.answers.append(answer)
+    def add_question(self, question_data):
+        self.questions.append(question_data['question'])
+        self.answers.append(question_data['answers'])
      
-     # take an index as an argument and remove the corresponding question and answer from the lists   
     def remove_question(self, index):
         del self.questions[index]
         del self.answers[index]
@@ -16,6 +14,30 @@ class Quiz:
     def take_quiz(self):
         score = 0
         for i in range(len(self.questions)):
-            answer = input()
-        
-        
+            # Display the question and answer choices
+            print(f"{self.questions[i]}")
+            for j, choice in enumerate(self.answers[i]):
+                print(f"{j+1}. {choice}")
+            
+            # Get the user's answer
+            answer_idx = int(input("Enter your answer (1-4): ")) - 1
+            
+            # Check if the answer is correct
+            if self.answers[i][answer_idx] == self.answers[i][0]:
+                score += 1
+        # Display the results
+        print(f"You got {score} out of {len(self.questions)} questions correct.")
+
+
+# Create a Quiz object with two questions and answers
+q = Quiz(["What color is the sky?", "What is the capital of France?"],
+         [["blue", "green", "red", "yellow"], ["Paris", "Madrid", "Rome", "Berlin"]])
+
+# Add a third question and answer
+q.add_question({
+    'question': 'What is the capital of Germany?',
+    'answers': ['Berlin', 'Paris', 'Madrid', 'Rome']
+})
+
+# Take the quiz
+q.take_quiz()
